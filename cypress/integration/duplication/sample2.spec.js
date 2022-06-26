07398cbc7d8d2b34b9d204eb33c216a0e7b122be
pedrovitorqa <pedro.paula@viannasempre.com.br>
2022-06-26 18:40:00 -0300

    Resolvendo duplicações de codigo

--- cypress/integration/duplication/sample2.spec.js
+++ cypress/integration/duplication/sample2.spec.js
@@ -8,30 +8,20 @@ describe('Code duplication bad practice - Sample 2', () => {
     cy.visit('https://hackernews-seven.vercel.app')
     cy.wait('@getStories')
 
-    cy.get('input[type="text"]')
-      .should('be.visible')
-      .and('have.value', 'redux')
-      .as('searchField')
-      .clear()
   })
 
-  it('searches for "reactjs"', () => {
-    cy.get('@searchField')
-      .type('reactjs{enter}')
+  const terms = ['reactjs', 'vuejs', 'angularjs']
 
-    cy.wait('@getStories')
+  terms.forEach(term => {
 
-    cy.get('.table-row')
-      .should('have.length', 100)
-  })
-
-  it('searches for "vuejs"', () => {
-    cy.get('@searchField')
-      .type('vuejs{enter}')
-
-    cy.wait('@getStories')
-
-    cy.get('.table-row')
-      .should('have.length', 100)
+    it(`searches for "${term}"`, () => {
+    
+      cy.search(term)
+  
+      cy.wait('@getStories')
+  
+      cy.get('.table-row')
+        .should('have.length', 100)
+    })
   })
 })
